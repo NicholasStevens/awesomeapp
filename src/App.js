a17e3c6ecf216e606d73d520cc4c9863d326e14d
SharkWolfMan <nick.stevens.86@gmail.com>
2022-01-24 21:21:50 +0100

    "final commit from ch1"

--- src/App.js
+++ src/App.js
@@ -1,39 +1,71 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Title from "./components/Title";
+import Pokemon from "./components/Pokemon";
+
+const all_pokemon = [
+  {
+    name: "Charizard",
+    weight: 90,
+    awesome: true,
+    terrifying: false,
+    abilities: ["Blaze", "Solar power", "Tough claws", "Drought"],
+  },
+  {
+    name: "Bulbasaur",
+    weight: 6.9,
+    awesome: true,
+    terrifying: false,
+    abilities: ["Overgrow", "Chlorophyll"],
+  },
+  {
+    name: "Mewtwo",
+    weight: 122,
+    awesome: true,
+    terrifying: true,
+    abilities: ["Pressure", "Unnerve", "Steadfast", "Insomnia"],
+  },
+  {
+    name: "Mega beedrill",
+    weight: 65,
+    awesome: false,
+    terrifying: true,
+    abilities: ["Intimidate", "Unnerve"],
+  },
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <main>
-          <Title />
-          <h1>Learning React: Week 2</h1>
-        </main>
-
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello World
-        </a>
-
-        <h1>First time using "React"</h1>
-        <p> Where will this show up?? </p>
-        <div>
-          {" "}
-          <button>hello world</button>{" "}
+      <main className="container">
+        <Title content="Some Simple Title" />
+        <div className="row">
+          {all_pokemon.map((pokemon, index) => (
+            <div key={index} className="col-md-6 col-lg-4">
+              <Pokemon
+                name={pokemon.name}
+                weight={pokemon.weight}
+                awesome={pokemon.awesome}
+                terrifying={pokemon.terrifying}
+                abilities={pokemon.abilities}
+              />
+            </div>
+          ))}
         </div>
-        <a href="wwww.google.com" target="_blank">
-          Click this link
-        </a>
-      </header>
+      </main>
+      <h1>Learning React: Week 2</h1>
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Hello World
+      </a>
     </div>
   );
 }
